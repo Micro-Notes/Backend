4e5e95308175fe6596afbd02a017e2c0dcfa13cd
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-03 21:26:41 -0500

    Eliminando codigo inecesario, corrigiendo modelo Note

--- Models/Note.cs
+++ Models/Note.cs
@@ -5,7 +5,7 @@ public class Note
     public int Id { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
-    public DateOnly? DateUpdate { get; set; }
+    public DateOnly? Date_update { get; set; }
     public string? Category { get; set; }
     public string? Status { get; set; }
     public int? IdUser { get; set; }
